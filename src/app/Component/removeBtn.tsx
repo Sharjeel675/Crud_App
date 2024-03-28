d2fd696666bc0504986004d8e407d8a38b1dca91
Sharjeel675 <94467411+Sharjeel675@users.noreply.github.com>
2024-03-28 19:33:00 +0500

    a

--- src/app/Component/removeBtn.tsx
+++ src/app/Component/removeBtn.tsx
@@ -5,6 +5,8 @@ const RemoveBtn = () => {
   return (
     <div>
       <button className='text-red-400'><HiOutlineTrash size={24}/></button>
+      <h1>This is Remove Button</h1>
+      <p>Hello</p>
     </div>
   )
 }
